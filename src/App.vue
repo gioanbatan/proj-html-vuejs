556e781bdee9c745d6245c7c6e1d05a28fd7987e
gioanbatan <giovannibassani@hotmail.com>
2022-11-17 16:32:35 +0100

    Add: Basic Structure

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,50 @@
 </script>
 
 <template>
+    <!-- Header -->
+    <!-- Header top -->
+    <!-- /Header top -->
 
+    <!-- Header bottom -->
+    <!-- /Header bottom -->
+    <!-- /Header -->
+
+    <!-- Main -->
+    <!-- CTA -->
+    <!-- /CTA -->
+
+    <!-- Merits -->
+    <!-- /Merits -->
+
+    <!-- Browse categories -->
+    <!-- /Browse categories -->
+
+    <!-- Jumbotron -->
+    <!-- /Jumbotron -->
+
+    <!-- Browse Recent Coureses -->
+    <!-- /Browse Recent Coureses -->
+
+    <!-- Proud section -->
+    <!-- /Proud section -->
+
+    <!-- Subscrive Newsletter -->
+    <!-- /Subscrive Newsletter -->
+
+    <!-- Browse Popular Courses -->
+    <!-- /Browse Popular Courses -->
+
+    <!-- Banners section -->
+    <!-- /Banners section -->
+
+    <!-- Quote -->
+    <!-- /Quote -->
+    <!-- /Main -->
+
+    <!-- Footer -->
+    <!-- /Footer -->
 </template>
 
 <style lang="scss">
-
+@use "./styles/general.scss" as *;
 </style>
