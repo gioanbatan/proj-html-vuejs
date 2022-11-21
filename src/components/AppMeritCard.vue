2bf99be905171d75c38826f00c378c7d67356ee0
gioanbatan <giovannibassani@hotmail.com>
2022-11-21 16:30:50 +0100

    Add: AppMerits and AppMeritCard nearly completed

---
+++ src/components/AppMeritCard.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    name: "AppMeritCard",
+    props: {
+        icon: String,
+        title: String,
+        text: String
+    }
+}
+</script>  
+
+<template>
+    <section class="merit-card p-4">
+        <i :class="`d-block pb-4 ${icon}`"></i>
+        <h5>{{ title }}</h5>
+        <p>{{ text }}</p>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+@use "../styles/partials/variables" as *;
+
+
+.merit-card {
+    background-color: $col-light-blue;
+    width: 100%;
+    height: 100%;
+
+    i {
+        font-size: 3rem;
+        color: $col-std-blue;
+    }
+
+    h5 {
+        font-size: 0.9rem;
+    }
+
+    p {
+        font-size: 0.8rem;
+        color: $col-gray;
+    }
+}
+
+.merit-card:hover {
+    transform: translate(0, -10px);
+
+    p {
+        color: $col-text;
+    }
+}
+</style>
--- src/components/AppMerits.vue
+++ src/components/AppMerits.vue
@@ -1,6 +1,11 @@
 <script>
+import AppMeritCard from "./AppMeritCard.vue";
+
 export default {
-    name: "AppMerits"
+    name: "AppMerits",
+    components: {
+        AppMeritCard
+    }
 }
 
 </script>
@@ -8,21 +13,21 @@ export default {
 <template>
     <section class="merits">
         <div class="container">
-            <div class="merits-content">
-                <div class="row row-cols-3">
-                    <div class="col ">
-                        COLONNA
-                    </div>
-                    <div class="col">
-                        COLONNA
-                    </div>
-                    <div class="col">
-                        COLONNA
-                    </div>
+            <div class="row gx-4">
+                <div class="col">
+                    <AppMeritCard :icon="'fa-solid fa-earth-americas'" :title="'Worlwide Recognize'"
+                        :text="'Our online courses certificates can be used around th world and also in most popular universiuties and companies.'" />
+                </div>
+                <div class="col">
+                    <AppMeritCard :icon="'fa-solid fa-display'" :title="'Mostly Online Learning (sd)'"
+                        :text="'Masterstudy online certificates can be obtained in a range of specialized areas and typically take about a year to complete.'" />
+                </div>
+                <div class="col">
+                    <AppMeritCard :icon="'fa-regular fa-calendar-check'" :title="'Graduate in as little as 1 Year'"
+                        :text="'Online post-graduate certificates are a popular way to develop your professional qualifications of Masterstudy'" />
                 </div>
             </div>
         </div>
-
     </section>
 
 
@@ -30,12 +35,9 @@ export default {
 
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
+@use "../styles/partials/variables" as *;
 
-.col {
-
-    // DEBUG
-    background-color: aqua;
-    border-radius: 10px;
-    height: 100px;
+.row {
+    min-height: 230px;
 }
 </style>
